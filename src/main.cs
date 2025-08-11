92cab80c8bd4cbf9f2ab05ff29694e8abf0934c3
Maurel Sagbo <maurel.sagbo@gmail.com>
2025-08-11 15:38:17 +0200

    The echo builtin

--- src/main.cs
+++ src/main.cs
@@ -11,12 +11,28 @@ while (true)
     var words = command?.Split(' ');
     if (words != null && words.Length > 0)
     {
-        string firstWord = words[0];
-        if (firstWord == "exit")
-            return 0;
+        var firstWord = words[0];
+        switch (firstWord)
+        {
+            case "exit":
+                return 0;
+            case "echo":
+            {
+                for (int i = 1; i < words.Length; i++) 
+                    Console.Write(words[i] + (i == words.Length - 1 ? "" : " "));
+            
+                Console.WriteLine();
+                break;
+            }
+            default:
+                Console.WriteLine($"{command}: command not found");
+                break;
+        }
     }
-    
-    Console.WriteLine($"{command}: command not found");
+    else
+    {
+        Console.WriteLine($"{command}: command not found");
+    }
 
     Console.Write("$ ");
     command = Console.ReadLine();
