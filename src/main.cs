7932fa3da267e451f164c94118e068bd20f25c00
Maurel Sagbo <maurel.sagbo@gmail.com>
2025-08-12 13:28:19 +0200

    Testing run a program

--- src/main.cs
+++ src/main.cs
@@ -8,16 +8,16 @@ while (true)
 
     // Wait for user input
     var input = Console.ReadLine();
-    
+
     if (input == null)
         continue;
-    
+
     var words = input.Split(' ');
     if (words.Length > 0)
     {
         var command = words[0];
         var commandArgs = input[command.Length..].Trim();
-        
+
         switch (command)
         {
             case "exit":
@@ -34,59 +34,28 @@ while (true)
                 }
                 else
                 {
-                    var path = Environment.GetEnvironmentVariable("PATH");
-                    var pathDirs = path?.Split(Path.PathSeparator);
-                    
-                    var found = false;
-
-                    if (pathDirs != null)
-                    {
-                        foreach (var dir in pathDirs)
-                        {
-                            var fullPath = Path.Join(dir, commandArgs);
-                            if (File.Exists(fullPath) && HasExecutePermission(fullPath))
-                            {
-                                found = true;
-                                Console.WriteLine($"{commandArgs} is {fullPath}");
-                                break;
-                            }
-                        }
-                    }
-                    
-                    if(found == false)
-                        Console.WriteLine($"{commandArgs}: not found");
+                    var found = TryGetCommandDir(commandArgs, out _, out var fullPath);
+                    Console.WriteLine(found ? $"{commandArgs} is {fullPath}" : $"{commandArgs}: not found");
                 }
+
                 break;
             default:
-                var path1 = Environment.GetEnvironmentVariable("PATH");
-                var pathDirs1 = path1?.Split(Path.PathSeparator);
+                var foundExe = TryGetCommandDir(commandArgs, out var dir, out _);
 
-                var found1 = false;
                 var currentDirectory = Directory.GetCurrentDirectory();
 
-                if (pathDirs1 != null)
+                if (foundExe)
                 {
-                    foreach (var dir in pathDirs1)
-                    {
-                        var fullPath = Path.Join(dir, command);
-                        if (File.Exists(fullPath) && HasExecutePermission(fullPath))
-                        {
-                            found1 = true;
-                            //Console.WriteLine(fullPath);
-                            //Console.WriteLine(commandArgs);
-                            Directory.SetCurrentDirectory(dir);
-                            var process = Process.Start(command, commandArgs);
-                            process.WaitForExit();
-                            
-                            break;
-                        }
-                    }
-
+                    Directory.SetCurrentDirectory(dir ?? string.Empty); // Dir can not be null here
+                    var process = Process.Start(command, commandArgs);
+                    process.WaitForExit();
                     Directory.SetCurrentDirectory(currentDirectory);
                 }
+                else
+                {
+                    Console.WriteLine($"{input}: command not found");
+                }
 
-                if (found1 == false)
-                    Console.WriteLine($"{commandArgs}: not found");
                 break;
         }
     }
@@ -101,4 +70,28 @@ bool HasExecutePermission(string filePath)
     var fileInfo = new FileInfo(filePath);
     var unixFileMode = fileInfo.UnixFileMode;
     return (unixFileMode & UnixFileMode.UserExecute) != 0;
+}
+
+bool TryGetCommandDir(string command, out string? dir, out string? fullPath)
+{
+    var path = Environment.GetEnvironmentVariable("PATH");
+    var pathDirectories = path?.Split(Path.PathSeparator);
+
+    dir = null;
+    fullPath = null;
+
+    if (pathDirectories == null)
+        return false;
+
+    foreach (var directory in pathDirectories)
+    {
+        fullPath = Path.Join(directory, command);
+        if (File.Exists(fullPath) == false || HasExecutePermission(fullPath) == false)
+            continue;
+
+        dir = directory;
+        return true;
+    }
+
+    return false;
 }
